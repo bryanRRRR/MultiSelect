fae39be7a7d862e32cf61a2e5b6b94669dbb8f7b
生生 <417143914@qq.com>
2018-03-09 18:14:49 +0800

    Initial Commit

---
+++ multiSelect(Organization)DemoTests/multiSelect_Organization_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  multiSelect_Organization_DemoTests.swift
+//  multiSelect(Organization)DemoTests
+//
+//  Created by 生生 on 2018/3/9.
+//  Copyright © 2018年 生生. All rights reserved.
+//
+
+import XCTest
+@testable import multiSelect_Organization_Demo
+
+class multiSelect_Organization_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
